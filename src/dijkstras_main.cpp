e98a0227655e4e5062f37a81ae73d96c7a8fc9f4
Zoey Zhou <zhiruz5@uci.edu>
2025-03-15 01:28:04 +0000

    finish the dijkstras.main

---
+++ src/dijkstras_main.cpp
@@ -0,0 +1,22 @@
+#include "dijkstras.h"
+
+int main() {
+    try {
+        Graph G;
+        file_to_graph("graph.txt", G);
+        
+        vector<int> previous;
+        vector<int> distances = dijkstra_shortest_path(G, 0, previous);
+        
+        for (int i = 0; i < G.numVertices; i++) {
+            vector<int> path = extract_shortest_path(distances, previous, i);
+            print_path(path, distances[i]);
+        }
+        
+    } catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
+    
+    return 0;
+}
