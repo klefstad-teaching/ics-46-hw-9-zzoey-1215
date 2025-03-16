fb59c908a725034327a12a67c252b6d3b5edd863
Zoey Zhou <zhiruz5@uci.edu>
2025-03-16 04:43:35 +0000

    adjust the dijkstras.cpp

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -49,14 +49,13 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
-        cout << "No path exists" << endl;
+        cout << "\nTotal cost is " << total << endl;
         return;
     }
     
-    cout << "Path (cost = " << total << "): ";
     for (size_t i = 0; i < path.size(); i++) {
         cout << path[i];
-        if (i < path.size() - 1) cout << " -> ";
+        if (i < path.size() - 1) cout << " ";
     }
-    cout << endl;
+    cout << " \nTotal cost is " << total << endl;
 }
