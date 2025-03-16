ba520a4bb2275a0c105c4bbbcb43a69e24c5b0fd
Zoey Zhou <zhiruz5@uci.edu>
2025-03-16 06:20:37 +0000

    adjust the function to handle the infite loop error

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -45,44 +45,35 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
     queue<vector<string>> ladder_queue;
     set<string> visited;
     
-    vector<string> initial_ladder = {begin_word};
-    ladder_queue.push(initial_ladder);
+    ladder_queue.push({begin_word});
     visited.insert(begin_word);
     
-    const size_t MAX_LADDER_LENGTH = 50;  
+    set<string> filtered_words;
+    for (const string& word : word_list) {
+        if (abs((int)word.length() - (int)begin_word.length()) <= 1 &&
+            abs((int)word.length() - (int)end_word.length()) <= 1) {
+            filtered_words.insert(word);
+        }
+    }
     
     while (!ladder_queue.empty()) {
         vector<string> current_ladder = ladder_queue.front();
         ladder_queue.pop();
         
-        if (current_ladder.size() >= MAX_LADDER_LENGTH) {
-            continue;
-        }
-        
         string last_word = current_ladder.back();
         
-        for (const string& word : word_list) {
-            if (abs((int)word.length() - (int)last_word.length()) > 1) {
-                continue;
+        for (const string& word : filtered_words) {
+            if (!visited.count(word) && is_adjacent(last_word, word)) {
+                vector<string> new_ladder = current_ladder;
+                new_ladder.push_back(word);
+                
+                if (word == end_word) {
+                    return new_ladder;
+                }
+                
+                visited.insert(word);
+                ladder_queue.push(new_ladder);
             }
-            
-            if (visited.count(word)) {
-                continue;
-            }
-            
-            if (!is_adjacent(last_word, word)) {
-                continue;
-            }
-            
-            vector<string> new_ladder = current_ladder;
-            new_ladder.push_back(word);
-            
-            if (word == end_word) {
-                return new_ladder;
-            }
-            
-            visited.insert(word);
-            ladder_queue.push(new_ladder);
         }
     }
     
